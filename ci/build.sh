88e2dd702ef04514068387f7b1fb22b3978cfdd0
ffrancis <ffrancis@qti.qualcomm.com>
2025-05-23 11:27:00 +0530

    CI: Add initial GitHub Actions
    Includes the following components:
    
    1. aws_s3_exchanger (composite action):
       - Supports two modes: upload and download
       - Uploads files to S3 with dynamic paths based on GitHub context
       - Generates pre-signed URLs for uploaded files
       - Outputs a single URL for uploaded files
       - Uploads presigned_url.txt as an artifact in upload mode
    
    2. build (workflow):
       - Uses a Docker image to build project artifacts
       - Clones the meta-ar repository and applies necessary patches
       - Builds the project inside a Docker container
       - Creates a tarball of the build directory and uploads it as an artifact
    
    3. build_docker_image (composite action):
       - Clones the ar-image repository
       - Builds the Docker image using the Dockerfile from ar-image repository
    
    4. loading (composite action):
       - Parses build arguments from a configuration file
    
    5. Reusable build workflow:
       - Pulls SDK files from AWS S3 and runs the SDK script
       - Builds the project using the Docker image and specified build arguments
       - Cleans up the workspace after the build
    
    6. pre_merge_build (workflow):
       - Runs builds on pull request events to ensure code quality before merging
    
    7. process_image (workflow):
       - Extracts and mounts the pre-compiled image
       - Copies build files to the mounted image
       - Creates a tar image for qcomflash directory and uploads it to S3
    
    8. test (workflow):
       - Triggers LAVA tests on the build
       - Downloads pre-signed URLs and submits test jobs
    
    This commit establishes a basic docker-based CI pipeline for audioreach-pal
    builds and automated artifact handling via AWS S3
    
    Signed-off-by: Febin Francis <quic_ffrancis@quicinc.com>

---
+++ ci/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
+# Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+set -ex
+echo "Running build script..."
+# Build/Compile audioreach-graphservices
+source ${GITHUB_WORKSPACE}/install/environment-setup-armv8-2a-poky-linux
+
+# make sure we are in the right directory
+cd ${GITHUB_WORKSPACE}
+
+autoreconf -Wcross --verbose --install --force --exclude=autopoint
+autoconf --force
+
+# Run the configure script with the specified arguments
+./configure \${BUILD_ARGS} --with-glib --with-syslog
+# make
+make DESTDIR=${GITHUB_WORKSPACE}/build install
---
+++ ci/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+#
+# Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+# This file is part of the Audioreach CI configuration.
+# It defines the repositories and patches used in the CI process.
+REPOS=(
+  "arpal-lx|https://github.com/Audioreach/arpal-lx.git"
+)
+

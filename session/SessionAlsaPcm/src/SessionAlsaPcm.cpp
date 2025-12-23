CodeLinaro-mirror/le_platform_vendor_qcom_opensource_arpal-lx

pal: HW Timestamp PR enablement
Port of PR enablement changes for HW TimeStamp
from HIDL AHAL to AIDL AHAL

Change-Id: I42b24f9481134f6d70a9b9feff1fa0b5fd4bf8cf
Signed-off-by: Raghu Ballappa Bankapur <quic_rbankapu@quicinc.com>

session/SessionAlsaPcm/src/SessionAlsaPcm.cpp
@@ -2978,8 +2978,14 @@ int SessionAlsaPcm::connectSessionDevice(Stream* streamHandle, pal_stream_type_t
 
 int SessionAlsaPcm::read(Stream *s, struct pal_buffer *buf, int * size)
 {
-    int status = 0, bytesRead = 0, bytesToRead = 0, offset = 0, pcmReadSize = 0;
+    int status = 0, bytesRead = 0, bytesToRead = 0, offset = 0, pcmReadSize = 0, rc = 0;
     struct pal_stream_attributes sAttr = {};
+
+    uint64_t timestamp = 0;
+    const char *control = "bufTimestamp";
+    const char *stream = "PCM";
+    struct mixer_ctl *ctl;
+    std::ostringstream CntrlName;
 
     PAL_VERBOSE(LOG_TAG, "Enter")
     status = s->getStreamAttributes(&sAttr);
@@ -3016,6 +3022,30 @@ int SessionAlsaPcm::read(Stream *s, struct pal_buffer *buf, int * size)
             PAL_ERR(LOG_TAG, "Failed to read data %d bytes read %d", status, pcmReadSize);
             break;
         }
+        if (!bytesRead && buf->ts) {
+            CntrlName << stream << pcmDevIds.at(0) << " " << control;
+            ctl = mixer_get_ctl_by_name(mixer, CntrlName.str().data());
+            if (!ctl) {
+                PAL_ERR(LOG_TAG, "fail to fetch hardware timestamp, Invalid mixer control: %s\n", CntrlName.str().data());
+                bytesRead += pcmReadSize;
+                continue;
+            }
+
+            rc = mixer_ctl_get_array(ctl, (void *)&timestamp, sizeof(uint64_t));
+            if (0 != rc) {
+                PAL_ERR(LOG_TAG, "fail to fetch hardware timestamp, Get timestamp failed, rc = %d", rc);
+                bytesRead += pcmReadSize;
+                continue;
+            }
+            /* timestamp is splitted into sec and nsec,
+               it is not the exact conversion but the fraction is converted to nsec
+            */
+            buf->ts->tv_sec = timestamp / 1000000;
+            buf->ts->tv_nsec = (timestamp - buf->ts->tv_sec * 1000000) * 1000;
+            PAL_VERBOSE(LOG_TAG, "Timestamp %llu, tv_sec = %lu, tv_nsec = %lu",
+                       (long long)timestamp, buf->ts->tv_sec, buf->ts->tv_nsec);
+        }
+
 
         bytesRead += pcmReadSize;
     }

AudioReach/audioreach-pal

Port of PR enablement changes for HW TimeStamp from HIDL AHAL to AIDL
AHAL
Signed-off-by: Chiluka Rohith <rchiluka@qti.qualcomm.com>

session/SessionAlsaPcm/src/SessionAlsaPcm.cpp
@@ -1920,8 +1920,14 @@ int SessionAlsaPcm::connectSessionDevice(Stream* streamHandle, pal_stream_type_t
 
 int SessionAlsaPcm::read(Stream *s, struct pal_buffer *buf, int * size)
 {
-    int status = 0, bytesRead = 0, bytesToRead = 0, offset = 0, pcmReadSize = 0;
+    int status = 0, bytesRead = 0, bytesToRead = 0, offset = 0, pcmReadSize = 0, rc = 0;
     struct pal_stream_attributes sAttr = {};
+
+    uint64_t timestamp = 0;
+    const char *control = "bufTimestamp";
+    const char *stream = "PCM";
+    struct mixer_ctl *ctl;
+    std::ostringstream CntrlName;
 
     PAL_VERBOSE(LOG_TAG, "Enter")
     status = s->getStreamAttributes(&sAttr);
@@ -1959,6 +1965,30 @@ int SessionAlsaPcm::read(Stream *s, struct pal_buffer *buf, int * size)
             PAL_ERR(LOG_TAG, "Failed to read data %d bytes read %d", status, pcmReadSize);
             break;
         }
+        if (!bytesRead && buf->ts) {
+            CntrlName << stream << pcmDevIds.at(0) << " " << control;
+            ctl = mixer_get_ctl_by_name(mixer, CntrlName.str().data());
+            if (!ctl) {
+                PAL_DBG(LOG_TAG, "fail to fetch hardware timestamp, Invalid mixer control: %s\n", CntrlName.str().data());
+                bytesRead += pcmReadSize;
+                continue;
+            }
+
+            rc = mixer_ctl_get_array(ctl, (void *)&timestamp, sizeof(uint64_t));
+            if (0 != rc) {
+                PAL_DBG(LOG_TAG, "fail to fetch hardware timestamp, Get timestamp failed, rc = %d", rc);
+                bytesRead += pcmReadSize;
+                continue;
+            }
+            /* timestamp is splitted into sec and nsec,
+               it is not the exact conversion but the fraction is converted to nsec
+            */
+            buf->ts->tv_sec = timestamp / 1000000;
+            buf->ts->tv_nsec = (timestamp - buf->ts->tv_sec * 1000000) * 1000;
+            PAL_VERBOSE(LOG_TAG, "Timestamp %llu, tv_sec = %lu, tv_nsec = %lu",
+                       (long long)timestamp, buf->ts->tv_sec, buf->ts->tv_nsec);
+        }
+
 
         bytesRead += pcmReadSize;
     }